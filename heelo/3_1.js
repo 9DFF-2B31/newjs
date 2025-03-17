c1d27ea83117af72b814e1660715b1ea12b31cb0
Sneha Gupta <136964344+9DFF-2B31@users.noreply.github.com>
2025-03-17 20:06:30 +0000

    container config

---
+++ heelo/3_1.js
@@ -0,0 +1,21 @@
+let val=3
+let negval=-val
+console.log(negval)
+
+
+
+let str1="hello"
+let str2="sneha"
+let str3=str1+str2
+console.log(str3)
+console.log(1+'2')
+console.log('1'+'2')
+console.log(1+2+'3')
+console.log('1'+2+2)
+console.log(+true)
+console.log(+"")
+// console.log(true+)//invalid
+
+let n1,n2,n3
+n1=n2=n3=2+3
+console.log([n1,n2,n3])
