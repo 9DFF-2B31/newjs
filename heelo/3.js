1c84d81b2b041bc4392f60a86c4c46487ecbe364
Sneha Gupta <136964344+9DFF-2B31@users.noreply.github.com>
2025-03-17 19:50:06 +0000

    container config

---
+++ heelo/3.js
@@ -0,0 +1,57 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof (score));
+
+//conversion wouldnt give the correct value for elements
+let val=Number(score)
+console.log(typeof val);
+console.log(val);
+console.log("....................")
+
+
+
+let score1=null
+console.log(typeof score1);
+console.log(typeof (score1));
+let val1=Number(score1)
+console.log(typeof val1);
+console.log(val1);
+
+console.log("....................")
+
+let score2=undefined
+console.log(typeof score2);
+console.log(typeof (score2));
+let val2=Number(score2)
+console.log(typeof val2);
+console.log(val2);
+
+console.log("....................")
+
+let score3=true
+console.log(typeof score3);
+console.log(typeof (score3));
+let val3=Number(score3)
+console.log(typeof val3);
+console.log(val3);
+
+console.log("....................")
+
+let score4="sueee"
+console.log(typeof score4);
+console.log(typeof (score4));
+let val4=Number(score4)
+console.log(typeof val4);
+console.log(val4);//string cant be change  to number
+
+
+console.log("complex....................")
+
+
+let ishi="sneha"
+let boolf=Boolean(ishi)
+console.log(boolf)
+
+let ishi1=""
+let boolf1=Boolean(ishi1)
+console.log(boolf1)
