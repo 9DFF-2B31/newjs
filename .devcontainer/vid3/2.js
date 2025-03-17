15ef351e966aaa24921c4a5dcd9425a8c0ee929f
Sneha Gupta <136964344+9DFF-2B31@users.noreply.github.com>
2025-03-17 19:22:47 +0000

    container config

---
+++ .devcontainer/vid3/2.js
@@ -0,0 +1,14 @@
+"use strict" //treat all js as newer strict
+// alert(3+3)
+let n='name'
+let age=18
+let islogin=true
+//number =>2 to power 53(range)
+//string =>'', ""
+//boolean=>true/false
+//null =>standalone value
+//undefined=>value not assigned
+// symbol=>unique
+
+console.log(typeof(age))
+
